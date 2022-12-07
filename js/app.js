047741cc177b2c0c5b7bff586f7c5eefd0b50312
Yuko Bolak <yukobolak@gmail.com>
2022-12-07 18:02:46 -0500

    Updated html and scss.
    Finished base scss for hoe and about sections.

--- js/app.js
+++ js/app.js
@@ -3,9 +3,14 @@
 /* ============================================= */
 
 const hamMenu = document.querySelector('.hamburger-menu');
-const offScreenMenu = document.querySelector('.off-screen-menu');
+const offScreenMenu = document.querySelector('.off-screen-container');
+const menuLink = document.querySelector('.menu-link');
 
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
+});
+
+menuLink.addEventListener('click', () => {
+    menuLink.classList.toggle('active');
 });
