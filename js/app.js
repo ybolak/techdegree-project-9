413cddcaa9763f03e9b6b93d84295a88e9732476
Yuko Bolak <yukobolak@gmail.com>
2022-12-13 17:57:31 -0500

    Updated html, scss and js
    Changed background and added animation to the navbar.

--- js/app.js
+++ js/app.js
@@ -2,15 +2,25 @@
 /*                  Nav Menu                     */
 /* ============================================= */
 
-const hamMenu = document.querySelector('.hamburger-menu');
-const offScreenMenu = document.querySelector('.off-screen-container');
-const menuLink = document.querySelector('.menu-link');
+const hamMenu = document.querySelector(".hamburger-menu");
+const offScreenMenu = document.querySelector(".off-screen-container");
+const menuLink = document.querySelector(".menu-link");
 
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
-});
+function toggleOffScreenMenu(element) {
+  element.addEventListener("click", () => {
+    hamMenu.classList.toggle("active");
+    offScreenMenu.classList.toggle("active");
+  });
+}
 
-menuLink.addEventListener('click', () => {
-    menuLink.classList.toggle('active');
+toggleOffScreenMenu(hamMenu);
+toggleOffScreenMenu(offScreenMenu);
+
+// hamMenu.addEventListener('click', () => {
+//     hamMenu.classList.toggle('active');
+//     offScreenMenu.classList.toggle('active');
+// });
+
+menuLink.addEventListener("click", () => {
+  menuLink.classList.toggle("active");
 });
