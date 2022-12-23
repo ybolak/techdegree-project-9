482e40566662e13418de4ad6d5432a47abf0a32c
Yuko Bolak <yukobolak@gmail.com>
2022-12-23 17:31:49 -0500

    Updated html, scss and js
    Added description to each projects and active class to nav bar on scroll, changed the main logo.

--- js/app.js
+++ js/app.js
@@ -19,3 +19,26 @@ toggleOffScreenMenu(offScreenMenu);
 menuLink.addEventListener("click", () => {
   menuLink.classList.toggle("active");
 });
+
+// Nav active class on scroll
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll(".menu-link");
+
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (window.pageYOffset >= sectionTop - 200) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((item) => {
+    item.classList.remove("active");
+    if (item.textContent.toLowerCase().includes(current)) {
+      item.classList.add("active");
+    }
+  });
+};
